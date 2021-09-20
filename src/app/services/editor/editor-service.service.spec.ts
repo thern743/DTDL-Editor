8af744f9bf19462c29283e3e2ea0ef07a766e9ae
Tony Hernandez <tonyhernandez743@gmail.com>
2021-09-20 13:52:42 -0600

    Fixed naming

--- src/app/services/editor/editor-service.service.spec.ts
+++ src/app/services/editor/editor-service.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { EditorService } from './editor-service.service';
 
-describe('MainEditorServiceService', () => {
+describe('EditorService', () => {
   let service: EditorService;
 
   beforeEach(() => {
