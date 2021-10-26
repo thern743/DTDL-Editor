95f8ff775b24cbdfa89e79f86d2c347c02c44b9e
Tony Hernandez <tonyhernandez743@gmail.com>
2021-10-26 10:06:32 -0600

    Slide toggle for 'writieable' property fields.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { ReactiveFormsModule } from '@angular/forms';
-import { MainEditorComponent } from './main-editor/main-editor.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Angular Material
 import { MatListModule } from '@angular/material/list';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
@@ -16,29 +13,29 @@ import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
+import { MainEditorComponent } from './main-editor/main-editor.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PreviewPanelComponent } from './preview-panel/preview-panel.component';
 import { CommandPayloadComponent } from './command/command.component';
 import { JsonLdPipe } from './filters/jsonld.pipe';
 import { TelemetryComponent } from './telemetry/telemetry.component';
 import { PropertyComponent } from './property/property.component';
-
-import { MatIconModule } from '@angular/material/icon';
 import { InterfaceComponent } from './interface/interface.component';
 import { RelationshipComponent } from './relationship/relationship.component';
 import { ComponentComponent } from './component/component.component';
-
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatTreeModule } from '@angular/material/tree';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { ModelTreeComponent } from './model-tree/model-tree.component';
-import { MatDividerModule } from '@angular/material/divider';
 import { FolderSelectComponent } from './file-select/file-select.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { ErrorSnackbarComponent } from './error-snackbar/error-snackbar.component';
-
 import { ClipboardModule } from '@angular/cdk/clipboard';
 
 @NgModule({
@@ -78,7 +75,8 @@ import { ClipboardModule } from '@angular/cdk/clipboard';
     MatFormFieldModule,
     FormsModule,
     MatSnackBarModule,
-    ClipboardModule
+    ClipboardModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
