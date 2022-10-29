bf17b44c439ab15fa5119b21f75c1840cb87b59a
Tony Hernandez <tonyhernandez743@gmail.com>
2022-10-28 19:17:14 -0600

    Fixed ability to edit schemas for map schema type.

--- src/app/map-schema/map-schema.component.ts
+++ src/app/map-schema/map-schema.component.ts
@@ -22,8 +22,8 @@ export class MapSchemaComponent implements OnInit {
   private _formBuilder: FormBuilder;
   private _validationService: ValidationService;
   public dialog: MatDialog;
-  public keySchemaTypes: Map<string, AbstractCapabilityFormControl<AbstractCapabilityModel> | undefined>;
-  public valueSchemaTypes: Map<string, AbstractCapabilityFormControl<AbstractCapabilityModel> | undefined>;
+  public keySchemaTypes: Map<string, AbstractCapabilityFormControl<AbstractCapabilityModel>>;
+  public valueSchemaTypes: Map<string, AbstractCapabilityFormControl<AbstractCapabilityModel>>;
 
   constructor(schemaService: SchemaService,
     formBuilder: FormBuilder, 
@@ -36,20 +36,12 @@ export class MapSchemaComponent implements OnInit {
     this._validationService = validationService;
     this.dialog = dialog;
     this.map = new MapSchemaFormControl(data, this._formBuilder, this._validationService, this.dialog);
-    this.keySchemaTypes = new Map<string, AbstractCapabilityFormControl<AbstractCapabilityModel>>();
-    this.valueSchemaTypes = new Map<string, AbstractCapabilityFormControl<AbstractCapabilityModel>>();
-    this.mapKeysAndValues();
+    this.keySchemaTypes = this.schemaService.getSchemaTypesFormControls();
+    this.valueSchemaTypes = this.schemaService.getSchemaTypesFormControls();
   }
 
   public ngOnInit(): void { 
     this.map.subscribeModelToForm();
-  }
-
-  private mapKeysAndValues(): void {
-    this.schemaService.schemaFactory.formRegistry.forEach((value, key) => {
-      this.keySchemaTypes.set(key, undefined);
-      this.valueSchemaTypes.set(key, undefined);
-    });
   }
 
   public changeMapKey($event: MatSelectChange): void {
@@ -81,84 +73,4 @@ export class MapSchemaComponent implements OnInit {
     // TODO: This is a hack. Figure out a better solution.
     (<ISchemaEditor><unknown>form).openSchemaEditor(this.map.mapValue.form, schemaName);
   }
-
-  // TODO: New these up at selection time.
-  // public getValueSchemaTypes(): void {
-  //   this.schemaService.schemaFactory.registerModel("arrayMap", () => new MapValueCapabilityModel<ArraySchemaCapabilityModel>("dtmi:com:Example:MyArray;1")); 
-  //   this.schemaService.schemaFactory.registerModel("booleanMap", () => new MapValueCapabilityModel<BooleanSchemaCapabilityModel>("dtmi:com:Example:MyBoolean;1")); 
-  //   this.schemaService.schemaFactory.registerModel("dateMap", () => new MapValueCapabilityModel<DateSchemaCapabilityModel>("dtmi:com:Example:MyDate;1")); 
-  //   this.schemaService.schemaFactory.registerModel("dateTimeMap", () => new MapValueCapabilityModel<DateTimeSchemaCapabilityModel>("dtmi:com:Example:MyDateTime;1")); 
-  //   this.schemaService.schemaFactory.registerModel("doubleMap", () => new MapValueCapabilityModel<DoubleSchemaCapabilityModel>("dtmi:com:Example:MyDouble;1")); 
-  //   this.schemaService.schemaFactory.registerModel("durationMap", () => new MapValueCapabilityModel<DurationSchemaCapabilityModel>("dtmi:com:Example:MyDuration;1")); 
-  //   this.schemaService.schemaFactory.registerModel("enumMap", () => new MapValueCapabilityModel<EnumSchemaCapabilityModel>("dtmi:com:Example:MyEnum;1")); 
-  //   this.schemaService.schemaFactory.registerModel("floatMap", () => new MapValueCapabilityModel<FloatSchemaCapabilityModel>("dtmi:com:Example:MyFloat;1")); 
-  //   this.schemaService.schemaFactory.registerModel("integerMap", () => new MapValueCapabilityModel<IntegerSchemaCapabilityModel>("dtmi:com:Example:MyInteger;1")); 
-  //   this.schemaService.schemaFactory.registerModel("longMap", () => new MapValueCapabilityModel<LongSchemaCapabilityModel>("dtmi:com:Example:MyLong;1")); 
-  //   this.schemaService.schemaFactory.registerModel("mapMap", () => new MapValueCapabilityModel<MapSchemaCapabilityModel>("dtmi:com:Example:MyMap;1")); 
-  //   this.schemaService.schemaFactory.registerModel("objectMap", () => new MapValueCapabilityModel<ObjectSchemaCapabilityModel>("dtmi:com:Example:MyObject;1")); 
-  //   this.schemaService.schemaFactory.registerModel("stringMap", () => new MapValueCapabilityModel<StringSchemaCapabilityModel>("dtmi:com:Example:MyString;1")); 
-  //   this.schemaService.schemaFactory.registerModel("timeMap", () => new MapValueCapabilityModel<TimeSchemaCapabilityModel>("dtmi:com:Example:MyTime;1"));
-
-  //   this.schemaService.schemaFactory.registerForm("arrayMap", () => new MapValueFormControl(this.getModel("arrayMap"), this._formBuilder, this._validationService, this.dialog)); 
-  //   this.schemaService.schemaFactory.registerForm("booleanMap", () => new MapValueCapabilityModel<BooleanSchemaCapabilityModel>("dtmi:com:Example:MyBoolean;1")); 
-  //   this.schemaService.schemaFactory.registerForm("dateMap", () => new MapValueCapabilityModel<DateSchemaCapabilityModel>("dtmi:com:Example:MyDate;1")); 
-  //   this.schemaService.schemaFactory.registerForm("dateTimeMap", () => new MapValueCapabilityModel<DateTimeSchemaCapabilityModel>("dtmi:com:Example:MyDateTime;1")); 
-  //   this.schemaService.schemaFactory.registerForm("doubleMap", () => new MapValueCapabilityModel<DoubleSchemaCapabilityModel>("dtmi:com:Example:MyDouble;1")); 
-  //   this.schemaService.schemaFactory.registerForm("durationMap", () => new MapValueCapabilityModel<DurationSchemaCapabilityModel>("dtmi:com:Example:MyDuration;1")); 
-  //   this.schemaService.schemaFactory.registerForm("enumMap", () => new MapValueCapabilityModel<EnumSchemaCapabilityModel>("dtmi:com:Example:MyEnum;1")); 
-  //   this.schemaService.schemaFactory.registerForm("floatMap", () => new MapValueCapabilityModel<FloatSchemaCapabilityModel>("dtmi:com:Example:MyFloat;1")); 
-  //   this.schemaService.schemaFactory.registerForm("integerMap", () => new MapValueCapabilityModel<IntegerSchemaCapabilityModel>("dtmi:com:Example:MyInteger;1")); 
-  //   this.schemaService.schemaFactory.registerForm("longMap", () => new MapValueCapabilityModel<LongSchemaCapabilityModel>("dtmi:com:Example:MyLong;1")); 
-  //   this.schemaService.schemaFactory.registerForm("mapMap", () => new MapValueCapabilityModel<MapSchemaCapabilityModel>("dtmi:com:Example:MyMap;1")); 
-  //   this.schemaService.schemaFactory.registerForm("objectMap", () => new MapValueCapabilityModel<ObjectSchemaCapabilityModel>("dtmi:com:Example:MyObject;1")); 
-  //   this.schemaService.schemaFactory.registerForm("stringMap", () => new MapValueCapabilityModel<StringSchemaCapabilityModel>("dtmi:com:Example:MyString;1")); 
-  //   this.schemaService.schemaFactory.registerForm("timeMap", () => new MapValueCapabilityModel<TimeSchemaCapabilityModel>("dtmi:com:Example:MyTime;1"));
-
-  //   // return new Map<string, AbstractCapabilityFormControl<ICapabilityModel>>([
-  //   //   ["array", new MapValueFormControl(new MapValueCapabilityModel<ArraySchemaCapabilityModel>("dtmi:com:Example:MyArray;1"), this._formBuilder, this._validationService, this.dialog)], 
-  //   //   ["boolean", new MapValueFormControl(new MapValueCapabilityModel<ArraySchemaCapabilityModel>("dtmi:com:Example:MyBoolean;1"), this._formBuilder, this._validationService, this.dialog)], 
-  //   //   ["date", new MapValueFormControl(new MapValueCapabilityModel<ArraySchemaCapabilityModel>("dtmi:com:Example:MyDate;1"), this._formBuilder, this._validationService, this.dialog)], 
-  //   //   ["dateTime", new MapValueFormControl(new MapValueCapabilityModel<ArraySchemaCapabilityModel>("dtmi:com:Example:MyDateTime;1"), this._formBuilder, this._validationService, this.dialog)], 
-  //   //   ["double", new MapValueFormControl(new MapValueCapabilityModel<ArraySchemaCapabilityModel>("dtmi:com:Example:MyDouble;1"), this._formBuilder, this._validationService, this.dialog)], 
-  //   //   ["duration", new MapValueFormControl(new MapValueCapabilityModel<ArraySchemaCapabilityModel>("dtmi:com:Example:MyDuration;1"), this._formBuilder, this._validationService, this.dialog)], 
-  //   //   ["enum", new MapValueFormControl(new MapValueCapabilityModel<ArraySchemaCapabilityModel>("dtmi:com:Example:MyEnum;1"), this._formBuilder, this._validationService, this.dialog)], 
-  //   //   ["float", new MapValueFormControl(new MapValueCapabilityModel<ArraySchemaCapabilityModel>("dtmi:com:Example:MyFloat;1"), this._formBuilder, this._validationService, this.dialog)], 
-  //   //   ["integer", new MapValueFormControl(new MapValueCapabilityModel<ArraySchemaCapabilityModel>("dtmi:com:Example:MyInteger;1"), this._formBuilder, this._validationService, this.dialog)], 
-  //   //   ["long", new MapValueFormControl(new MapValueCapabilityModel<ArraySchemaCapabilityModel>("dtmi:com:Example:MyLong;1"), this._formBuilder, this._validationService, this.dialog)], 
-  //   //   ["map", new MapValueFormControl(new MapValueCapabilityModel<ArraySchemaCapabilityModel>("dtmi:com:Example:MyMap;1"), this._formBuilder, this._validationService, this.dialog)], 
-  //   //   ["object", new MapValueFormControl(new MapValueCapabilityModel<ArraySchemaCapabilityModel>("dtmi:com:Example:MyObject;1"), this._formBuilder, this._validationService, this.dialog)], 
-  //   //   ["string", new MapValueFormControl(new MapValueCapabilityModel<ArraySchemaCapabilityModel>("dtmi:com:Example:MyString;1"), this._formBuilder, this._validationService, this.dialog)], 
-  //   //   ["time", new MapValueFormControl(new MapValueCapabilityModel<ArraySchemaCapabilityModel>("dtmi:com:Example:MyTime;1"), this._formBuilder, this._validationService, this.dialog)]  
-  //   // ]);
-  // }
-
-  // public getSchemaTypesFormControls(): Map<string, () => AbstractCapabilityFormControl<ICapabilityModel>> {
-  //   return new Map<string, () => AbstractCapabilityFormControl<ICapabilityModel>>([
-  //     ["array", () => new ArraySchemaFormControl(new ArraySchemaCapabilityModel("dtmi:com:Example:MyArray;1"), this._formBuilder, this._validationService, this.dialog)], 
-  //     ["boolean", () => new BooleanSchemaFormControl(new BooleanSchemaCapabilityModel("dtmi:com:Example:MyBoolean;1"), this._formBuilder, this._validationService, this.dialog)], 
-  //     ["date", () => new DateSchemaFormControl(new DateSchemaCapabilityModel("dtmi:com:Example:MyDate;1"), this._formBuilder, this._validationService, this.dialog)], 
-  //     ["dateTime", () => new DateTimeSchemaFormControl(new DateTimeSchemaCapabilityModel("dtmi:com:Example:MyDateTime;1"), this._formBuilder, this._validationService, this.dialog)], 
-  //     ["double", () => new DoubleSchemaFormControl(new DoubleSchemaCapabilityModel("dtmi:com:Example:MyDouble;1"), this._formBuilder, this._validationService, this.dialog)], 
-  //     ["duration", () => new DurationSchemaFormControl(new DurationSchemaCapabilityModel("dtmi:com:Example:MyDuration;1"), this._formBuilder, this._validationService, this.dialog)], 
-  //     ["enum", () => new EnumSchemaFormControl(new EnumSchemaCapabilityModel("dtmi:com:Example:MyEnum;1"), this._formBuilder, this._validationService, this.dialog)], 
-  //     ["float", () => new FloatSchemaFormControl(new FloatSchemaCapabilityModel("dtmi:com:Example:MyFloat;1"), this._formBuilder, this._validationService, this.dialog)], 
-  //     ["integer", () => new IntegerSchemaFormControl(new IntegerSchemaCapabilityModel("dtmi:com:Example:MyInteger;1"), this._formBuilder, this._validationService, this.dialog)], 
-  //     ["long", () => new LongSchemaFormControl(new LongSchemaCapabilityModel("dtmi:com:Example:MyLong;1"), this._formBuilder, this._validationService, this.dialog)], 
-  //     ["map", () => new MapSchemaFormControl(new MapSchemaCapabilityModel("dtmi:com:Example:MyMap;1"), this._formBuilder, this._validationService, this.dialog)], 
-  //     ["object", () => new ObjectSchemaFormControl(new ObjectSchemaCapabilityModel("dtmi:com:Example:MyObject;1"), this._formBuilder, this._validationService, this.dialog)], 
-  //     ["string", () => new StringSchemaFormControl(new StringSchemaCapabilityModel("dtmi:com:Example:MyString;1"), this._formBuilder, this._validationService, this.dialog)], 
-  //     ["time", () => new TimeSchemaFormControl(new TimeSchemaCapabilityModel("dtmi:com:Example:MyTime;1"), this._formBuilder, this._validationService, this.dialog)]  
-  //   ]);
-  // }
-
-  // private getKeySchemaTypes(): Map<string, AbstractCapabilityFormControl<MapKeyCapabilityModel>> {
-  //   return new Map<string, AbstractCapabilityFormControl<MapKeyCapabilityModel>>([
-  //     ["string", new MapKeyFormControl(new MapKeyCapabilityModel("dtmi:com:Example:MyMapKey;1"), this._formBuilder, this._validationService, this.dialog)]
-  //   ]);
-  // }
-
-  // private getModel(name: string): AbstractCapabilityModel | undefined {
-  //   let form = this.schemaService.schemaFactory.createModel(name);
-  //   return form;
-  // } 
 }
